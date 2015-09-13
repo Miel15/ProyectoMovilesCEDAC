6476200f6cacbf97884ec4e7b1a067d5fd2bd2cc
carlossebastianloredogomez <sebas_1625@hotmail.com>
2015-09-13 13:37:24 -0500

    Initial Commit

---
+++ MovilesCedac/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovilesCedac
+//
+//  Created by Sebastián Loredo on 13/09/15.
+//  Copyright (c) 2015 Carlos Sebastián Loredo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
